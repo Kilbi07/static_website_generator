c44d364caaafd239477bb9b4ae45301bfe9cd718
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-11-03 15:41:58 +0100

    Update enum

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL = "normal text"
-    BOLD = "bold text"
-    ITALIC = "italic text"
-    CODE = "code text"
-    LINK = "links"
-    IMAGE = "images"
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self,text,text_type,url = None):
