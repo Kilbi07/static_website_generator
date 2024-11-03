8ab8c9dd8bf1001f6d33a110936c903a95640b4d
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-11-03 16:01:24 +0100

    Update main script

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from textnode import *
+
+def main():
+    my_text_node = TextNode("This is a text node","bold", "https://www.boot.dev")
+    print(my_text_node)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
