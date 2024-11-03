2c70f00c629df5b9b6996f85da0b2dbd17de833b
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-11-03 15:37:48 +0100

    add Textnode

--- src/textnode.py
+++ src/textnode.py
@@ -5,3 +5,17 @@ class TextType(Enum):
     BOLD = "bold text"
     ITALIC = "italic text"
     CODE = "code text"
+    LINK = "links"
+    IMAGE = "images"
+
+class TextNode:
+    def __init__(self,text,text_type,url = None):
+        self.text = text
+        self.text_type = text_type.value
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.url == other.url and self.text_type == other.text_type
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
